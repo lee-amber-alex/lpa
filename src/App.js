807d4e9aac2647fc36bb12430040452090aa684b
Amber Lee <lee.amber.alex@gmail.com>
2021-12-15 12:56:28 -0800

    Added Container for Appian Interface.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
 import './App.css';
+import Home from "./components/Home/home";
 
 function App() {
   return (
     <div >
       <h1>HomePage</h1>
+      <Home/>
     </div>
   );
 }
---
+++ src/components/Home/home.js
@@ -0,0 +1,17 @@
+// import { Container } from "react-bootstrap";
+import "../Styles/home.css"
+
+function Home() {
+
+    return (
+        <div>
+            <div className="container">
+
+<div>Test</div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Home;
